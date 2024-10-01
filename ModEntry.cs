8c35e5b810845ab800663034dda4f79c6e9a4b90
Max Vollmer <maxvollmer@users.noreply.github.com>
2024-10-01 20:18:28 +0200

    PickUpClutter mod project and source code

---
+++ ModEntry.cs
@@ -0,0 +1,37 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace PickUpClutter
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.DayStarted += DayStartedHandler;
+            helper.Events.Player.InventoryChanged += InventoryChangedHandler;
+        }
+
+        private void InventoryChangedHandler(object? sender, InventoryChangedEventArgs e)
+        {
+            foreach (var i in e.Added)
+            {
+                if (i is StardewValley.Object o)
+                {
+                    o.GetContextTags().Add("placeable");
+                }
+            }
+        }
+
+        private void DayStartedHandler(object? sender, DayStartedEventArgs e)
+        {
+            foreach (var location in Game1.locations)
+            {
+                foreach (var o in location.objects.Values)
+                {
+                    o.IsSpawnedObject = true;
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+A mod for Stardew Valley that allows you to pick up and relocate rocks, twigs, and weeds.
+
+I created this mod while fixing my DeepWoods ﻿mod and encountering a bug that allowed picking up rocks, twigs, and weeds. After figuring out the cause of the bug, a user in the comments suggested making a mod that adds the bug as a feature. And here we are.
+
+I made a Youtube video where you can watch me create this mod: https://www.youtube.com/watch?v=tKsL62TW820
+
+Get the mod from Nexusmods here: https://www.nexusmods.com/stardewvalley/mods/28279
